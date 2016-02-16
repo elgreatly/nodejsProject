201ccc077694fa6fcc114dba2789daba118042a2
Abdelrhman ElGreatly <info.elgreatly@gmail.com>
2016-02-16 12:21:28 +0200

    Update movies.js

--- routes/movies.js
+++ routes/movies.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var neo4j = require('node-neo4j');
-var databaseUsername = 'neo4j';
-var databasePassword = '01115192714';
+var databaseUsername = 'neo4jUsername';
+var databasePassword = 'neo4jPassword';
 var host = 'localhost';
-var databasePort = '7474';
+var databasePort = 'port';
 db = new neo4j('http://' + databaseUsername + ':' + databasePassword + '@' + host + ':'+ databasePort);
 
 router.get('/getmovies', function(req, res, next) {
